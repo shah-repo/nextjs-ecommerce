fe8d72615cffe8e284b6dba3cd50ca71f9ce877f
shah-cloud-III <sfahed@enlil.com>
2023-11-19 18:14:44 +0530

    Merge Anonymous cart to the User aligned cart while login, functionality added.

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,3 +1,4 @@
+import { mergeAnonymousCartWithUserCart } from "@/lib/db/cart";
 import prisma from "@/lib/db/prisma";
 import { env } from "@/lib/env";
 import { PrismaAdapter } from "@auth/prisma-adapter";
@@ -16,6 +17,24 @@ export const nextAuthOptions: NextAuthOptions = {
       clientSecret: env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  callbacks: {
+    // it will be trigered whenever we return session from the DB
+    // Here we can add what data we want in user object
+    session({ session, user }) {
+      // This way we can take the user id from DB and add it to the session user oject.
+      // But typescript doesn't recognise an id field on this session user
+      // So we have to configure this type, we have to extend the user type
+      session.user.id = user.id;
+      return session;
+    },
+  },
+  //  it will be called every time when certain auth operations happen.
+  events: {
+    // This will be called right after signin, but before returned to home page
+    async signIn({ user }) {
+      await mergeAnonymousCartWithUserCart(user.id);
+    },
+  },
 };
 
 const handler = NextAuth(nextAuthOptions);
--- src/lib/db/cart.tsx
+++ src/lib/db/cart.tsx
@@ -1,6 +1,8 @@
 import prisma from "./prisma";
 import { cookies } from "next/dist/client/components/headers";
-import { Cart, Prisma } from "@prisma/client";
+import { Cart, CartItem, Prisma } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { nextAuthOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export type CartWithProducts = Prisma.CartGetPayload<{
   include: { items: { include: { product: true } } };
@@ -16,13 +18,23 @@ export interface ShoppingCart extends CartWithProducts {
 }
 
 export async function getCart(): Promise<ShoppingCart | null> {
-  const localCartId = cookies().get("localCartId")?.value;
-  const cart = localCartId
-    ? await prisma.cart.findUnique({
-        where: { id: localCartId },
-        include: { items: { include: { product: true } } },
-      })
-    : null;
+  const session = await getServerSession(nextAuthOptions);
+  let cart: CartWithProducts | null = null;
+
+  if (session) {
+    cart = await prisma.cart.findFirst({
+      where: { userId: session.user.id },
+      include: { items: { include: { product: true } } },
+    });
+  } else {
+    const localCartId = cookies().get("localCartId")?.value;
+    cart = localCartId
+      ? await prisma.cart.findUnique({
+          where: { id: localCartId },
+          include: { items: { include: { product: true } } },
+        })
+      : null;
+  }
 
   if (!cart) {
     return null;
@@ -40,10 +52,22 @@ export async function getCart(): Promise<ShoppingCart | null> {
 }
 
 export async function createCart(): Promise<ShoppingCart> {
-  const newCart = await prisma.cart.create({ data: {} });
+  const session = await getServerSession(nextAuthOptions);
 
-  // Note:- Needs encryption + secure settings in real production apps
-  cookies().set("localCartId", newCart.id);
+  let newCart: Cart;
+
+  if (session) {
+    // session.user.id will show error like 'Property 'id' does not exist on type user'
+    // By default 'id' not content in user object, next-auth doesn't return it by default.
+    // we have to add 'id' ourself inside the route handler file,
+    // in NextAuthOptions config object by adding callbacks field.
+    newCart = await prisma.cart.create({ data: { userId: session?.user.id } });
+  } else {
+    newCart = await prisma.cart.create({ data: {} });
+
+    // Note:- Needs encryption + secure settings in real production apps
+    cookies().set("localCartId", newCart.id);
+  }
 
   return {
     ...newCart,
@@ -51,4 +75,76 @@ export async function createCart(): Promise<ShoppingCart> {
     subTotal: 0,
     items: [],
   };
+}
+
+// We want to call this method just right after the login, so before the page open the merging already happened
+export async function mergeAnonymousCartWithUserCart(userId: string) {
+  const localCartId = cookies().get("localCartId")?.value;
+
+  const localCart = localCartId
+    ? await prisma.cart.findUnique({
+        where: { id: localCartId },
+        include: { items: true },
+      })
+    : null;
+
+  if (!localCart) return;
+
+  const userCart = await prisma.cart.findFirst({
+    where: { userId },
+    include: { items: true },
+  });
+
+  // For DB roll back functionality we can use prisma $transaction function,
+  // the function passes prismaClient as argument, so that we can use it for DB transaction inside the same function
+  // Every operation we call on this prisma client will be part of transaction.
+  // A DB transaction is a process where multiple operations can run but if one of them fails, the whole transaction will be rolled back,
+  // and none of the cahnge will be applied, the below example is to execute transaction with prisma
+  await prisma.$transaction(async (tx) => {
+    if (userCart) {
+      const mergedCartItems = mergeCartItems(userCart.items, localCart.items);
+      // Now delete user cart items
+      await tx.cartItem.deleteMany({ where: { cartId: userCart.id } });
+      // Now create new merged cart items
+      await tx.cartItem.createMany({
+        data: mergedCartItems.map((cartItem) => ({
+          cartId: userCart.id,
+          productId: cartItem.productId,
+          quantity: cartItem.quantity,
+        })),
+      });
+    } else {
+      await tx.cart.create({
+        data: {
+          userId,
+          // it's a relation query in prisma, it will automatically be connected by adding the cart 'id' to these items.
+          items: {
+            createMany: {
+              data: localCart.items.map((cartItem) => ({
+                productId: cartItem.productId,
+                quantity: cartItem.quantity,
+              })),
+            },
+          },
+        },
+      });
+    }
+    // Now delete the local cart
+    await tx.cart.delete({ where: { id: localCart.id } });
+    cookies().set("localCartId", "");
+  });
+}
+
+function mergeCartItems(...cartItems: CartItem[][]) {
+  return cartItems.reduce((acc, items) => {
+    items.forEach((item) => {
+      const existingItem = acc.find((i) => i.productId === item.productId);
+      if (existingItem) {
+        existingItem.quantity += item.quantity;
+      } else {
+        acc.push(item);
+      }
+    });
+    return acc;
+  }, [] as CartItem[]);
 }
